b2baf382a1703194a448044a7fcdf2917e2e5b02
LordRyanII <ryanoliveiram2020@outlook.com>
2023-08-30 22:18:18 -0300

    Complementando rotas

--- frontend/src/Component/App.jsx
+++ frontend/src/Component/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Component/Template/Pages/Home/Telainicial";
 import Produtos from "../Component/Template/Pages/Produtos/Home/HomeProdutos";
+import About from '../Component/Template/Pages/About/About'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produtos/cafeexpresso" element={<Produtos />} />
+        <Route path="/contato/sobre" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Component/Template/Pages/About/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import header from "../../Header/Header";
+import Nav from "../../Nav/Nav";
+import Conteudo from './Conteudo'
+
+function About() {
+  return (
+    <main>
+      <header />
+      <Nav />
+      <Conteudo />
+    </main>
+  );
+}
+
+
+export default About
