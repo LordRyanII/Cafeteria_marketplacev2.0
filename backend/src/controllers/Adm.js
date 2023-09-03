bec7ddce7b891238fcba84dcc3391f258912a940
LordRyanII <ryanoliveiram2020@outlook.com>
2023-09-03 17:17:15 -0300

    Criando validação de senhas

--- backend/src/controllers/Adm.js
+++ backend/src/controllers/Adm.js
@@ -2,18 +2,25 @@ const CadastroSchemma = require('../../Models/Logins/LoginAdm/CadastroAdm');
 
 exports.CadastroAdm = async (req, res) => {
     const data = req.body;
-    console.log(data)
-    // Criptografa a senha
+    console.log(data);
     try {
-        const cadastro = new CadastroSchemma(data);
-        await cadastro.criptografia(); // Chama a função de criptografia
+        const verificaDados = (obj) => Object.values(obj).some(value => !value || value.trim() === "");
+        if (verificaDados(data)) {
+            return res.status(400).json({
+                status: 'Nok',
+                mensage: 'Há dados não preenchidos'
+            });
+        } else {
+            const cadastro = new CadastroSchemma(data);
+            await cadastro.criptografia(); // Chama a função de criptografia
 
-        res.status(200).json({
-            status: 'Ok!',
-            message: "Gestor cadastrado com sucesso",
-        });
+            res.status(200).json({
+                status: 'Ok!',
+                message: "Gestor cadastrado com sucesso",
+            });
+        }
     } catch (error) {
-        res.status(401).json({
+        res.status(500).json({
             status: 'Nok',
             message: "Erro ao realizar o cadastro: " + error.message,
         });
