998a6152115315af329964f40ce0b2f10a8af11a
LordRyanII <ryanoliveiram2020@outlook.com>
2023-09-18 00:01:42 -0300

    Feat: Adicionando card destaque

--- frontend/src/Component/Template/Pages/Cardapios/ConteudoCardapio.jsx
+++ frontend/src/Component/Template/Pages/Cardapios/ConteudoCardapio.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import './Cardapio.css'
+import "./Cardapio.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function ConteudoCardapio() {
   const [produtos, setProdutos] = useState([]);
+  const [primeiroProduto, setPrimeiroProduto] = useState(null);
 
   useEffect(() => {
     const urlBase = "http://localhost:3002/produtos/res/produtos/";
@@ -14,6 +17,10 @@ function ConteudoCardapio() {
       .then((response) => {
         const produtosData = response.data.produtos;
         setProdutos(produtosData);
+        // Defina o primeiro produto como o primeiro da lista.
+        if (produtosData.length > 0) {
+          setPrimeiroProduto(produtosData[0]);
+        }
       })
       .catch((error) => {
         console.error("Erro ao buscar produtos:", error);
@@ -22,15 +29,24 @@ function ConteudoCardapio() {
 
   return (
     <div className="containerCardapio">
-        <h2 style={{margin: 'auto', textAlign: 'center'}}>Cardápio</h2>
+      <h2 style={{ margin: "auto", textAlign: "center" }} className="displayTitulo">Cardápio <FontAwesomeIcon icon={faCoffee} /></h2><br />
+      {primeiroProduto && (
+        <Card className="primeiroProduto">
+          <Card.Img variant="top" src={primeiroProduto.image} />
+          <Card.Body>
+            <Card.Text style={{textAlign: 'center'}}>
+              {primeiroProduto.descricao + ' .confira abaixo'}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+      <br />
+
       <div className="row">
-        {produtos.map((produto) => (
+        {produtos.map((produto, index) => (
           <div key={produto._id} className="col-md-4">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src={produto.image}
-              />
+            <Card style={{ width: "18rem" }} className={index === 0 ? "primeiroProduto" : ""}>
+              <Card.Img variant="top" src={produto.image} />
               <Card.Body>
                 <Card.Title>{produto.produto}</Card.Title>
                 <Card.Text>{produto.descricao}</Card.Text>
@@ -38,7 +54,16 @@ function ConteudoCardapio() {
                 <Card.Text>
                   Preço: R${produto.preco_compra.toFixed(2)}
                 </Card.Text>
-                <Button style={{margin: 'auto', textAlign: 'center', backgroundColor: 'green', color: 'white'}}> Conferir</Button>
+                <Button
+                  style={{
+                    margin: "auto",
+                    textAlign: "center",
+                    backgroundColor: "green",
+                    color: "white",
+                  }}
+                >
+                  Conferir
+                </Button>
               </Card.Body>
             </Card>
           </div>
